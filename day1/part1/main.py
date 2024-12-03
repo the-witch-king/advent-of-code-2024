13880ba30c96f321af0f11bc43cd667afec2752c
Michael Churilov <michael.churilov@gmail.com>
2024-12-03 07:52:35 -0500

    Day 1

---
+++ day1/part1/main.py
@@ -0,0 +1,12 @@
+import csv
+
+reports = list(csv.reader(open("../input.txt"), delimiter=" "))
+
+left_reports = sorted([int(r[0]) for r in reports])
+right_reports = sorted([int(r[1]) for r in reports])
+
+ordered_reports = zip(left_reports, right_reports)
+
+result = sum([abs(int(r[0]) - int(r[1])) for r in ordered_reports])
+
+print(result)
---
+++ day1/part2/main.py
@@ -0,0 +1,16 @@
+import csv
+
+reports = list(csv.reader(open("../input.txt"), delimiter=" "))
+
+left_reports = [int(r[0]) for r in reports]
+right_reports = [int(r[1]) for r in reports]
+
+similarity_scores = {l: 0 for l in left_reports}
+
+for r in right_reports:
+    if r in similarity_scores:
+        similarity_scores[r] = similarity_scores[r] + 1
+
+final_score = sum(l * similarity_scores[l] for l in left_reports)
+
+print(final_score)
